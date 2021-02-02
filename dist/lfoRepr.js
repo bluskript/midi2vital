6d90d8e73cd89f8814f75da3babbe8099d23fb3f
Floris <flvlams@gmail.com>
2021-02-02 23:28:29 +0100

    Updated note lookup
    Shortened note name to LFO pitch function

--- dist/lfoRepr.js
+++ dist/lfoRepr.js
@@ -1,45 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+let notes = { 'C': 0, 'D': 2, 'E': 4, 'F': 5, 'G': 7, 'A': 9, 'B': 11 };
 function pitchToLFO(note) {
-    let pitch = parseInt(note.substr(note.length - 1), 10) * 12;
-    switch (note.slice(0, -1)) {
-        case "C":
-            pitch += 0;
-            break;
-        case "C#":
-            pitch += 1;
-            break;
-        case "D":
-            pitch += 2;
-            break;
-        case "D#":
-            pitch += 3;
-            break;
-        case "E":
-            pitch += 4;
-            break;
-        case "F":
-            pitch += 5;
-            break;
-        case "F#":
-            pitch += 6;
-            break;
-        case "G":
-            pitch += 7;
-            break;
-        case "G#":
-            pitch += 8;
-            break;
-        case "A":
-            pitch += 9;
-            break;
-        case "A#":
-            pitch += 10;
-            break;
-        case "B":
-            pitch += 11;
-            break;
-    }
+    let pitch = parseInt(note.substr(note.length - 1), 10) * 12 + notes[note[0]];
+    if (note[1] == '#')
+        pitch++;
     return 1 - pitch / 12 / 8;
 }
 exports.pitchToLFO = pitchToLFO;
